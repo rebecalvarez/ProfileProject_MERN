5cef9afe9f5ef37858f49149ea45ffb8e4cbc56c
rebecalvarez <rebeca.dodero@gmail.com>
2019-05-22 20:28:24 -0500

    update build

---
+++ client/build/precache-manifest.d0bd2b5002eb7fa569c75f2714d3d899.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "40ae9968a6e1bdf30ee2",
+    "url": "/static/js/main.6e2813e5.chunk.js"
+  },
+  {
+    "revision": "bef7ce99eeae994974da",
+    "url": "/static/js/2.ffae726f.chunk.js"
+  },
+  {
+    "revision": "40ae9968a6e1bdf30ee2",
+    "url": "/static/css/main.212a5ddb.chunk.css"
+  },
+  {
+    "revision": "5091832322e681ad75c4fe9d9d96fa5f",
+    "url": "/index.html"
+  }
+];
